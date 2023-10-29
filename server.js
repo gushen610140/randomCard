2d0eb54c2700654a2a2ae9ef85667e06ce8a7491
gushen <w56038009@outlook.com>
2023-10-29 22:09:51 +0800

    2

--- server.js
+++ server.js
@@ -1,65 +1,23 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
 
-const mimes = {
-    'html': 'text/html',
-    'css': 'text/css',
-    'js': 'text/javascript',
-    'png': 'image/png',
-    'jpg': 'image/jpeg',
-    'gif': 'image/gif',
-    'mp4': 'vedio/mp4',
-    'mp3': 'vedio/mpeg',
-    'json': 'application/json',
-    'svg': 'image/svg+xml'
+const express = require('express')
+const bodyParser = require('body-parser')
 
-}
+const app = express()
 
-const server = http.createServer((request, response) => {
-    if (request.method !== 'GET') {
-        response.statusCode = 405
-        response.end('Error')
-        return;
-    }
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-    const { pathname } = new URL(request.url, 'http://127.0.0.1')
-    const root = __dirname + '/resources'
-    const filePath = root + pathname
+app.use(express.static(__dirname + '/resources'))
 
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            switch (err.code) {
-                case 'ENOENT':
-                    response.statusCode = 404
-                    response.end('404 Not Found')
-                    break
-                case 'EPERM':
-                    response.statusCode = 403
-                    response.end('Permission')
-                    break;
-                default:
-                    response.statusCode = 500
-                    response.end('UnkownError')
-            }
-            return;
-        }
-        const ext = path.extname(filePath).slice(1)
-        const type = mimes[ext]
-        if (type) {
-            if (type === 'html')
-                response.setHeader('content-type', type + ';charset=utf-8')
-            else
-                response.setHeader('content-type', type)
-        }
-        else {
-            response.setHeader('content-type', 'application/octet-stream')
-        }
-        response.statusCode = 200
-        response.end(data)
-    })
+app.post('/post', urlencodedParser, (req, res) => {
+    userObject = req.body
+    res.send(userObject.username + ' ' + userObject.password)
 })
 
-server.listen(80, () => {
-    console.log('server begin success')
+app.all('*', (req, res) => {
+    res.redirect('/index.html')
+})
+
+app.listen(80, () => {
+    console.log('success')
 })
